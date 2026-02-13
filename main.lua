5ef31664c2d5377ec42d9d480232ed08cae8f9fa
lwsh05314-commits <lwsh05314@gmail.com>
2026-02-13 22:01:41 +0300

    Delete main.luan

--- main.lua
+++
@@ -1,8 +0,0 @@
--- Velocicode Executor Test Script
-print("Velocicode Executor v1: Active")
-
-game:GetService("StarterGui"):SetCore("SendNotification", {
-	Title = "Velocicode",
-	Text = "Executor successfully attached and running!",
-	Duration = 5
-})
