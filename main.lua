e598065e9626918c23c59d05e8e155d0eb812730
lwsh05314-commits <lwsh05314@gmail.com>
2026-02-13 21:34:38 +0300

    Create Velocicode Executor test script
    Add a test script for Velocicode Executor with a notification.

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- Velocicode Executor Test Script
+print("Velocicode Executor v1: Active")
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Velocicode",
+	Text = "Executor successfully attached and running!",
+	Duration = 5
+})
